73da884782e4258a3fc13a12a3047c9a70b4affd
UBISOFT-ORG\agargasson <antoine.gargasson@ubisoft.com>
2020-03-02 15:00:58 +0100

    First player movement

---
+++ Assets/Scripts/CameraPlayer.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+public class CameraPlayer : MonoBehaviour 
+{
+    #region Public Fields
+    public Transform    myTransformToRotate = null;
+    public bool         myCanMoveAtStart = true;
+    #endregion
+
+    #region Private Fields
+    private Transform   myTransform = null;
+
+    private float       myVerticalSpeed = 100.0f;
+    private float       myHorizontalSpeed = 150.0f;
+    private float       myCurrentAngle = 0;
+
+    private bool        myInvertMouse = true;
+    private bool        myCanMove = true;
+    #endregion
+
+    #region Unity Methods
+    private void Start () 
+	{
+        myTransform = transform;
+        myCanMove = myCanMoveAtStart;
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+	
+	private void Update () 
+	{
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            myInvertMouse = !myInvertMouse;
+        }
+
+        if (!myCanMove) return;
+
+        float x = Input.GetAxis("Mouse X") * Time.deltaTime * myHorizontalSpeed;
+        float z = Input.GetAxis("Mouse Y") * Time.deltaTime * myVerticalSpeed;
+        if (z > 0 && myCurrentAngle < 89)
+            myCurrentAngle += z;
+        else if (z < 0 && myCurrentAngle > -89)
+            myCurrentAngle += z;
+        if (myCurrentAngle > 89) myCurrentAngle = 89;
+        else if (myCurrentAngle < -89) myCurrentAngle = -89;
+
+        myTransformToRotate.localRotation = Quaternion.Euler(-myCurrentAngle, 0, 0);
+
+        myTransform.Rotate(0, x, 0);
+    }
+    #endregion
+
+    #region Public Methods
+    public void SetCanMove(bool newState)
+    {
+        myCanMove = newState;
+    }
+    #endregion
+
+    #region Private Methods
+    #endregion
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Transform       myCamera = null;
+    [SerializeField]
+    private float           mySpeed = 2;
+
+    private Vector3         myDir = Vector3.zero;
+    private Rigidbody       myBody = null;
+
+    private void Start()
+    {
+        myBody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        myDir = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.Z))
+        {
+            myDir += myCamera.forward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            myDir -= myCamera.forward;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            myDir += myCamera.right;
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            myDir -= myCamera.right;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        myBody.MovePosition(myBody.position + myDir.normalized * mySpeed * Time.fixedDeltaTime);
+    }
+}
