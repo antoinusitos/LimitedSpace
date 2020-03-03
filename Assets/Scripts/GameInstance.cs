7aa9476507bda92540c6af58f91289dcbdfcc039
UBISOFT-ORG\agargasson <antoine.gargasson@ubisoft.com>
2020-03-03 13:10:04 +0100

    Added Photon and done menu and lobby scenes

---
+++ Assets/Scripts/GameInstance.cs
@@ -0,0 +1,30 @@
+﻿using Photon.Pun;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameInstance : MonoBehaviour
+{
+    private static GameInstance     myInstance = null;
+
+    [SerializeField]
+    private InputField              myNameInputField = null;
+
+    private string                  myName = "";
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        myInstance = this;
+    }
+
+    public static GameInstance GetInstance()
+    {
+        return myInstance;
+    }
+
+    public void SetPlayerName()
+    {
+        myName = myNameInputField.text;
+        PhotonNetwork.LocalPlayer.NickName = myName;
+    }
+}
