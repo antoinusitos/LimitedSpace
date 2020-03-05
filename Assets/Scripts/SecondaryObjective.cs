b298cd24ef532abb45dbf46465c64a70340e753c
UBISOFT-ORG\agargasson <antoine.gargasson@ubisoft.com>
2020-03-05 17:44:26 +0100

    Added side objectives

---
+++ Assets/Scripts/SecondaryObjective.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+public class SecondaryObjective : MonoBehaviour
+{
+    [SerializeField]
+    private Takeable    myObjectToBeInside = null;
+
+    [SerializeField]
+    private string      myText = "";
+
+    private bool        myObjectiveComplete = false;
+
+    private bool        myStateChanged = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Takeable takeable = other.GetComponentInParent<Takeable>();
+        if(takeable != null)
+        {
+            if(myObjectToBeInside == takeable)
+            {
+                if(!myObjectiveComplete)
+                {
+                    myStateChanged = true;
+                }
+
+                myObjectiveComplete = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Takeable takeable = other.GetComponentInParent<Takeable>();
+        if (takeable != null)
+        {
+            if (myObjectToBeInside == takeable)
+            {
+                if (myObjectiveComplete)
+                {
+                    myStateChanged = true;
+                }
+
+                myObjectiveComplete = false;
+            }
+        }
+    }
+
+    public bool GetObjectiveComplete()
+    {
+        return myObjectiveComplete;
+    }
+
+    public string GetMyText()
+    {
+        return myText;
+    }
+
+    public bool GetMyStateChanged()
+    {
+        bool state = myStateChanged;
+        myStateChanged = false;
+        return state;
+    }
+}
---
+++ Assets/Scripts/SecondaryObjectiveManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SecondaryObjectiveManager : MonoBehaviour
+{
+    private SecondaryObjective[] mySecondaryObjectives = null;
+
+    [SerializeField]
+    private GameObject              myUIToSpawn = null;
+    private GameObject[]            myUISpawned = null;
+
+    [SerializeField]
+    private Transform               myPanel = null;
+
+    private void Start()
+    {
+        mySecondaryObjectives = FindObjectsOfType<SecondaryObjective>();
+        myUISpawned = new GameObject[mySecondaryObjectives.Length];
+        
+        for(int i = 0; i < mySecondaryObjectives.Length; i++)
+        {
+            myUISpawned[i] = Instantiate(myUIToSpawn, myPanel);
+            myUISpawned[i].GetComponentInChildren<Text>().text = mySecondaryObjectives[i].GetMyText();
+        }
+
+    }
+
+    private void Update()
+    {
+        for(int i = 0; i < mySecondaryObjectives.Length; i++)
+        {
+            if(mySecondaryObjectives[i].GetMyStateChanged())
+            {
+                myUISpawned[i].GetComponentInChildren<Toggle>().isOn = mySecondaryObjectives[i].GetObjectiveComplete();
+            }
+        }
+    }
+}
