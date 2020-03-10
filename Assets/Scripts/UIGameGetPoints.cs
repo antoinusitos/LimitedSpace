c1b10a68fc35a952bcfeb219ab0de9d490251179
UBISOFT-ORG\agargasson <antoine.gargasson@ubisoft.com>
2020-03-10 13:45:27 +0100

    Added score and finish point and sound manager

--- Assets/Scripts/UIGameGetPoints.cs
+++ Assets/Scripts/UIGameGetPoints.cs
@@ -3,18 +3,23 @@ using UnityEngine.UI;
 
 public class UIGameGetPoints : MonoBehaviour
 {
-    private Text        myText = null;
+    private Text            myText = null;
 
-    [SerializeField]
-    private HouseCheck  myHouseCheck = null;
+    private HouseCheck[]    myHouseCheck = null;
 
     private void Start()
     {
         myText = GetComponent<Text>();
+        myHouseCheck = FindObjectsOfType<HouseCheck>();
     }
 
     void Update()
     {
-        myText.text = myHouseCheck.GetTotalPoints().ToString();
+        int score = 0;
+        for(int i = 0; i < myHouseCheck.Length; i++)
+        {
+            score += myHouseCheck[i].GetTotalPoints();
+        }
+        myText.text = score.ToString();
     }
 }
